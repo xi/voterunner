31129dfd45c46a3076baa3024801dee68baf6346
Tobias Bengfort <tobias.bengfort@gmx.net>
2013-03-14 10:23:45 +0100

    switch to postgresql

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 var express = require('express')
   , app = express.createServer()
   , io = require('socket.io').listen(app)
-  , sqlite3 = require('sqlite3').verbose()
-  , db = new sqlite3.Database('db.sqlite')
+  , pg = require('pg')
   , fs = require('fs')
   , log = io.log;
 
@@ -13,9 +12,6 @@ app.listen(port, function() {
 	log.info("Listening on " + port);
 });
 app.use(express.static('static'));
-
-db.run("CREATE TABLE IF NOT EXISTS state (topic TEXT, id TEXT, name TEXT, comment TEXT, delegate TEXT, UNIQUE (topic, id))");
-db.run("CREATE TABLE IF NOT EXISTS chat (topic TEXT, id TEXT, text TEXT, t INTEGER)");
 
 // routes
 function markdown(file, res) {
@@ -34,75 +30,94 @@ app.get('/:topic/', function (req, res) {
 	res.sendfile('tpl/app.html');
 });
 
-// socket.io
-io.sockets.on('connection', function (socket) {
-	socket.on('topic', function(topic) {
-		socket.set('topic', topic);
-		socket.join(topic);
-	});
+pg.connect(process.env.DATABASE_URL, function(err, db) {
+	if (err) {
+		log.error(err);
+	} else {
+		log.info('connected to database on ' + process.env.DATABASE_URL);
+	}
 
-	socket.on('getState', function() {
-		socket.get('topic', function(err, topic) {
-			db.all('SELECT * FROM state WHERE topic = :topic', {':topic': topic}, function(err, data) {
-				socket.emit('state', {'nodes': data});
-			});
-			db.all('SELECT * FROM chat WHERE topic = :topic ORDER BY t ASC', {':topic': topic}, function(err, data) {
-				socket.emit('state', {'chat': data});
-			});
-		});
-	});
+	db.query("CREATE TABLE IF NOT EXISTS state (topic TEXT, id TEXT, name TEXT, comment TEXT, delegate TEXT, UNIQUE (topic, id))");
+	db.query("CREATE TABLE IF NOT EXISTS chat (topic TEXT, id TEXT, text TEXT, t INTEGER)");
 
-	socket.on('createNode', function(id) {
-		socket.get('topic', function(err, topic) {
-			socket.broadcast.to(topic).emit('createNode', id);
-			var sql = "INSERT INTO state (id, name, comment, delegate, topic) VALUES (:id, 'anonymous', '', '', :topic)";
-			db.run(sql, {':id': id, ':topic': topic}, function(err) {
-				if (err) {
-					log.warn("failed to apply `createNode` to state:", err.toString());
-				}
+	// socket.io
+	io.sockets.on('connection', function (socket) {
+		socket.on('topic', function(topic) {
+			socket.set('topic', topic);
+			socket.join(topic);
+		});
+
+		socket.on('getState', function() {
+			socket.get('topic', function(err, topic) {
+				db.query('SELECT * FROM state WHERE topic = $1', [topic], function(err, result) {
+					if (err) {
+						log.warn('getState:state', err, result);
+					} else {
+						socket.emit('state', {'nodes': result.rows});
+					}
+				});
+				db.query('SELECT * FROM chat WHERE topic = $1 ORDER BY t ASC', [topic], function(err, result) {
+					if (err) {
+						log.warn('getState:chat', err, result);
+					} else {
+						socket.emit('state', {'chat': result.rows});
+					}
+				});
 			});
 		});
-	});
-	socket.on('rmNode', function(id) {
-		socket.get('topic', function(err, topic) {
-			socket.broadcast.to(topic).emit('rmNode', id);
-			var sql = "DELETE FROM state WHERE id = :id AND topic = :topic";
-			db.run(sql, {':id': id, ':topic': topic});
+
+		socket.on('createNode', function(id) {
+			socket.get('topic', function(err, topic) {
+				socket.broadcast.to(topic).emit('createNode', id);
+				var sql = "INSERT INTO state (id, name, comment, delegate, topic) VALUES ($1, 'anonymous', '', '', $2)";
+				db.query(sql, [id, topic], function(err) {
+					if (err) {
+						log.warn("failed to apply `createNode` to state:", err.toString());
+					}
+				});
+			});
 		});
-	});
-	socket.on('setNodeName', function(id, name) {
-		socket.get('topic', function(err, topic) {
-			socket.broadcast.to(topic).emit('setNodeName', id, name);
-			var sql = "UPDATE state SET name = :name WHERE id = :id AND topic = :topic";
-			db.run(sql, {':id': id, ':name': name, ':topic': topic});
+		socket.on('rmNode', function(id) {
+			socket.get('topic', function(err, topic) {
+				socket.broadcast.to(topic).emit('rmNode', id);
+				var sql = "DELETE FROM state WHERE id = $1 AND topic = $2";
+				db.query(sql, [id, topic]);
+			});
 		});
-	});
-	socket.on('setNodeComment', function(id, comment) {
-		socket.get('topic', function(err, topic) {
-			socket.broadcast.to(topic).emit('setNodeComment', id, comment);
-			var sql = "UPDATE state SET comment = :comment WHERE id = :id AND topic = :topic";
-			db.run(sql, {':id': id, ':comment': comment, ':topic': topic});
+		socket.on('setNodeName', function(id, name) {
+			socket.get('topic', function(err, topic) {
+				socket.broadcast.to(topic).emit('setNodeName', id, name);
+				var sql = "UPDATE state SET name = $3 WHERE id = $1 AND topic = $2";
+				db.query(sql, [id, topic, name]);
+			});
 		});
-	});
-	socket.on('setDelegate', function(id, delegate) {
-		socket.get('topic', function(err, topic) {
-			socket.broadcast.to(topic).emit('setDelegate', id, delegate);
-			var sql = "UPDATE state SET delegate = :delegate WHERE id = :id AND topic= :topic";
-			db.run(sql, {':id': id, ':v': delegate, ':topic': topic});
+		socket.on('setNodeComment', function(id, comment) {
+			socket.get('topic', function(err, topic) {
+				socket.broadcast.to(topic).emit('setNodeComment', id, comment);
+				var sql = "UPDATE state SET comment = $3 WHERE id = $1 AND topic = $2";
+				db.query(sql, [id, topic, comment]);
+			});
 		});
-	});
-	socket.on('rmDelegate', function(id) {
-		socket.get('topic', function(err, topic) {
-			socket.broadcast.to(topic).emit('rmDelegate', id);
-			var sql = "UPDATE state SET delegate = '' WHERE id = :id AND topic = :topic";
-			db.run(sql, {':id': id, ':topic': topic});
+		socket.on('setDelegate', function(id, delegate) {
+			socket.get('topic', function(err, topic) {
+				socket.broadcast.to(topic).emit('setDelegate', id, delegate);
+				var sql = "UPDATE state SET delegate = $3 WHERE id = $1 AND topic= $2";
+				db.query(sql, [id, topic, delegate]);
+			});
 		});
-	});
-	socket.on('chat', function(id, text, t) {
-		socket.get('topic', function(err, topic) {
-			socket.broadcast.to(topic).emit('chat', id, text, t);
-			var sql = "INSERT INTO chat (topic, id, text, t) VALUES (:topic, :id, :text, :t)";
-			db.run(sql, {':topic': topic, ':id': id, ':text': text, ':t': t});
+		socket.on('rmDelegate', function(id) {
+			socket.get('topic', function(err, topic) {
+				socket.broadcast.to(topic).emit('rmDelegate', id);
+				var sql = "UPDATE state SET delegate = '' WHERE id = $1 AND topic = $2";
+				db.query(sql, [id, topic]);
+			});
+		});
+		socket.on('chat', function(id, text, t) {
+			socket.get('topic', function(err, topic) {
+				socket.broadcast.to(topic).emit('chat', id, text, t);
+				var sql = "INSERT INTO chat (topic, id, text, t) VALUES ($2, $1, $3, $4)";
+				db.query(sql, [id, topic, text, t]);
+			});
 		});
 	});
 });
