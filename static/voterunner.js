a0ec975c29c33c3143491e8a60a2b85fde14a50f
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-08-06 12:45:20 +0200

    better esapeHTML

--- static/voterunner.js
+++ static/voterunner.js
@@ -72,12 +72,9 @@ function _rmParentHighlight(o) {
 }
 
 function escapeHTML(unsafe) {
-	return unsafe
-		.replace(/&/g, "&amp;")
-		.replace(/</g, "&lt;")
-		.replace(/>/g, "&gt;")
-		.replace(/"/g, "&quot;")
-		.replace(/'/g, "&#039;");
+	var div = document.createElement('div');
+	div.textContent = unsafe;
+	return div.innerHTML;
 }
 
 
