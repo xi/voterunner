54aca60c3704457eb7290f99650afd1d5ee94282
Martin Häcker <spamfaenger@gmx.de>
2016-09-09 11:36:14 +0200

    Add skeletal combining and scss calls.

--- bin/watch.py
+++ bin/watch.py
@@ -9,8 +9,12 @@ from livereload import Server, shell
 import formic
 
 EXCLUDES = ['**/vendor/**', '**/node_modules/**', '**/data/**']
-UNCOMPILED_FILES = formic.FileSet(include=['**.js', '**.css', '**.html', '**.md'], exclude=EXCLUDES + ['/app.js'])
-UNCOMPILED_FILES_REQUIRING_SERVER_RESTART = formic.FileSet(include=['/app.js'], exclude=EXCLUDES)
+UNCOMPILED_FILES = formic.FileSet(include=['**.css', '**.html', '**.md'], exclude=EXCLUDES)
+JS_APP_FILES = formic.FileSet(include=['**.js'], exclude=EXCLUDES +['/app.js', '/static/voterunner.js'])
+COMBINED_JS_APP_FILES = formic.FileSet(include=['/static/voterunner.js'])
+NODE_APP_FILES = formic.FileSet(include=['app.js'])
+SCSS_FILES = formic.FileSet(include=['**.scss'], exclude=EXCLUDES)
+CSS_FILES = formic.FileSet(include=['**.css'], exclude=EXCLUDES)
 
 server = Server()
 
@@ -21,9 +25,13 @@ def watch(file_set, shell_command=None, delay=None):
 def show(file_set):
     map(print, file_set)
 
-# show(UNCOMPILED_FILES_REQUIRING_SERVER_RESTART)
-watch(UNCOMPILED_FILES)
+show(NODE_APP_FILES)
 
+watch(JS_APP_FILES, shell('echo FIXME: how does the combining work? dependency missing?'), delay='forever')
+watch(COMBINED_JS_APP_FILES)
+watch(UNCOMPILED_FILES)
+watch(SCSS_FILES, shell('echo FIXME: which scss compilere shold be used? dependency missing?'), delay='forever')
+watch(CSS_FILES)
 
 shell('pg_ctl start -w -D data/postgres -o "-h localhost" -l /dev/null')()
 
@@ -35,9 +43,9 @@ SERVER_SETTINGS = dict(
 ENV = ' '.join(map('='.join, SERVER_SETTINGS.items()))
 NODE_COMMAND = shell('killall node; %(ENV)s nohup node app.js > /dev/null 2>&1 &' % locals(), shell='/bin/sh')
 NODE_COMMAND()
-watch(UNCOMPILED_FILES_REQUIRING_SERVER_RESTART, NODE_COMMAND, delay=1)
+watch(NODE_APP_FILES, NODE_COMMAND, delay=1)
 
 server.serve(liveport=35729)
 
 # shell('pg_ctl stop -D data/postgres')()
-shell('killall node')
+# shell('killall node')()
