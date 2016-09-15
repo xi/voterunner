aafb60479a78b2889cbe45a4431d1e099f46e02b
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-09-15 15:46:51 +0200

    make initialize_db.sh work on debian

--- bin/initialize_db.sh
+++ bin/initialize_db.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 cd "$(dirname "$0")/.."
-mkdir -p data/postgres
-if test ! -d  data/postgres/base; then
-	pg_ctl initdb -D data/postgres
-	pg_ctl start -w -D data/postgres -o "-h localhost"
-	createdb voterunner
-	pg_ctl stop -D data/postgres
+DB_DIR="$(pwd)/data/postgres"
+mkdir -p $DB_DIR
+if test ! -d "$DB_DIR/base"; then
+	pg_ctl initdb -D "$DB_DIR"
+	pg_ctl start -w -D "$DB_DIR" -o "-h localhost" -o "-k '$DB_DIR'"
+	createdb -h "$DB_DIR" voterunner
+	pg_ctl stop -D "$DB_DIR"
 fi
